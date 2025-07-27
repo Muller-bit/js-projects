6dfdafba742596d3e328165374db13e08d57879a
Mulualem <mulualemdarimo76@gmail.com>
2025-07-27 14:39:35 +0200

    Added project files

--- Role Playing Game/scripts.js
+++ Role Playing Game/scripts.js
@@ -17,11 +17,20 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+const locations = [
+  {
+    name: "town square",
+    "button text": ["Go to store", "Go to cave", "Fight dragon"]
+  }
+];
 // initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
+function update(location) {
+
+}
 
 function goTown() {
   button1.innerText = "Go to store";
@@ -30,7 +39,7 @@ function goTown() {
   button1.onclick = goStore;
   button2.onclick = goCave;
   button3.onclick = fightDragon;
-text.innerText = "You are in the town square. You see a sign that says \"Store\"."
+  text.innerText = "You are in the town square. You see a sign that says \"Store\".";
 }
 
 function goStore() {
